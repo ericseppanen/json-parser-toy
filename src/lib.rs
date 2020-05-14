30c1465116634003d11455bae64d0f3d397eb661
Eric Seppanen <eds@reric.net>
2020-05-13 23:42:26 -0700

    add output data structures
    Our parsers now return JsonBool and JsonNull.

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,43 @@
 use nom::{branch::alt, bytes::complete::tag, IResult};
+use nom::combinator::map;
 
-fn json_bool(input: &str) -> IResult<&str, &str> {
-    alt((
+#[derive(PartialEq, Debug)]
+pub enum JsonBool {
+    False,
+    True,
+}
+
+#[derive(PartialEq, Debug)]
+pub struct JsonNull {}
+
+fn json_bool(input: &str) -> IResult<&str, JsonBool> {
+    let parser = alt((
         tag("false"),
         tag("true")
-    ))
+    ));
+    map(parser, |s| {
+        match s {
+            "false" => JsonBool::False,
+            "true" => JsonBool::True,
+            _ => unreachable!(),
+        }
+    })
     (input)
 }
 
-fn json_null(input: &str) -> IResult<&str, &str> {
-    tag("null")
+fn json_null(input: &str) -> IResult<&str, JsonNull> {
+    map(tag("null"), |_| JsonNull {})
     (input)
 }
 
 #[test]
 fn test_bool() {
-    assert_eq!(json_bool("false"), Ok(("", "false")));
-    assert_eq!(json_bool("true"), Ok(("", "true")));
+    assert_eq!(json_bool("false"), Ok(("", JsonBool::False)));
+    assert_eq!(json_bool("true"), Ok(("", JsonBool::True)));
     assert!(json_bool("foo").is_err());
 }
 
 #[test]
 fn test_null() {
-    assert_eq!(json_null("null"), Ok(("", "null")));
+    assert_eq!(json_null("null"), Ok(("", JsonNull {})));
 }
